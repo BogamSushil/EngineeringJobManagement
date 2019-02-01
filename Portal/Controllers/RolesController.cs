872e73c20c1c2517917028dde27396941cb64f5d
Sushil <sushil.bogam@gmail.com>
2019-02-01 13:53:08 +0000

    firt draft

---
+++ Portal/Controllers/RolesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Portal.Models;
+
+namespace Portal.Controllers
+{
+ 
+    public class RolesController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        [HttpGet]
+        public ActionResult AssignRoles()
+        {                  
+            return View(UserManager.Users.Select(f=> new User { Email =f.Email, Name =f.UserName}).ToList());
+        }
+
+        // Assign: Roles
+        [HttpPost]
+        public ActionResult AssignRoles(string userId, Roles role)
+        {
+
+          //  var result = UserManager.AddToRoles(userId, new string[] { role.Value });
+            return View();
+        }
+    }
+}
---
+++ Portal/Models/Roles.cs
@@ -0,0 +1,17 @@
+﻿namespace Portal.Models
+{
+    public sealed class Roles
+    {
+        public static readonly Roles Administrator = new Roles("Administrator");
+        public static readonly Roles CallCenterAjent = new Roles("CallCenterAjent");
+        public static readonly Roles Supervisor = new Roles("Supervisor");
+        public static readonly Roles Engineer = new Roles("Engineer");
+
+        private Roles(string value)
+        {
+            this.Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
